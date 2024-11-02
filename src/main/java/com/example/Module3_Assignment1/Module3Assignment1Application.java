665b60762d93644f9647697ea1202fd66cfcaaf1
unknown <praveenps@live.com>
2024-11-02 16:43:48 +0530

    Initial Commit

---
+++ src/main/java/com/example/Module3_Assignment1/Module3Assignment1Application.java
@@ -0,0 +1,16 @@
+package com.example.Module3_Assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module3Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module3Assignment1Application.class, args);
+	}
+	public String hello() {//done by Main Branch
+	      return String.format("Hello  this is DevOps");
+	 }
+
+}
---
+++ src/test/java/com/example/Module3_Assignment1/Module3Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Module3_Assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module3Assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
